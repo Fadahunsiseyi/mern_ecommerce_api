eab73c03062a9584e38222d0d864ffb4abcc11ff
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-04 21:42:28 +0100

    add the user model and hash user  password

---
+++ models/UserModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+// Declare the Schema of the Mongo model
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+    },
+    lastName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+});
+
+
+userSchema.pre('save', async function(next){
+  this.password = await bcrypt.hash(this.password,12)
+  next()
+})
+
+userSchema.methods.isPasswordMatched = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+//Export the model
+module.exports = mongoose.model('User', userSchema);

03363994cb2bd12f53db96b9d59d67ffc6ce75c7
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-06 14:56:28 +0100

    add the login route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
-const { createUser } = require('../controllers/authController');
+const { createUser, loginUser } = require('../controllers/authController');
 const router = express.Router();
 
 
 router.post('/register',createUser)
+router.post('/login',loginUser)
 
 
 

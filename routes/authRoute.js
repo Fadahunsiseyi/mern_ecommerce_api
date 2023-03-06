70f13133d9dcfcf5244b0fc23a2f3cc0d85aff92
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-06 19:31:24 +0100

    add route to update user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,13 @@
 
 const express = require('express');
-const { createUser, loginUser } = require('../controllers/authController');
+const { createUser, loginUser, getAllUser, getAUser, deleteAUser, updateAUser } = require('../controllers/authController');
 const router = express.Router();
 
 
 router.post('/register',createUser)
 router.post('/login',loginUser)
+
+router.put('/:id',updateAUser)
 
 
 

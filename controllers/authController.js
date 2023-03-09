f22e0c25691efdb6a2260800f2c7ee7bd045ca5a
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-09 17:20:27 +0100

    add function to block and unblock user and is protected

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
 const generateToken = require("../config/generateToken.js");
 const User = require("../models/UserModel.js");
 const asyncHandler = require("express-async-handler");
+const jwt = require('jsonwebtoken')
+const validateId = require("../utils/validateMongodbId.js");
+const generateRefreshToken = require("../config/refreshToken.js");
 
 const createUser = asyncHandler(async (req, res) => {
   const { email } = req.body;
@@ -19,6 +22,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const findUser = await User.findOne({ email });
   if (findUser && (await findUser.isPasswordMatched(password))) {
+
     const { firstName, lastName, email, _id, mobile } = findUser;
     res.status(200).json({
       message: "User logged in successfully",
@@ -37,54 +41,117 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-const updateAUser = async (req, res) => {
-  try {
-    const {id} = req.params;
-    const {firstName, lastName, email, mobile} = req.body;
-    const updateUser = await User.findByIdAndUpdate(id,{
-      firstName,
-      lastName,
-      email,
-      mobile
-    },{
-      new: true,
-    })
-    res.json({updateUser})
-  } catch (error) {
-    throw new Error(error)
-  }
-}
 
-const getAllUser = asyncHandler(async (req,res) => {
-  try {
-    const allUsers = await User.find()
-    res.json({users: allUsers})
-  } catch (error) {
-    throw new Error(error)
-  }
+const handleRefreshToken = asyncHandler( async (req,res) =>{
+  const {refreshToken} = req.cookies
+  if(!refreshToken) throw new Error("No refresh token found in cookies");
+  const user = await User.findOne({refreshToken});
+  if(!user) throw new Error("No refresh token found in the database")
+  jwt.verify(refreshToken,process.env.JWT,(err,decoded) =>{
+    if(err || user.id !== decoded.id){
+      throw new Error('There is something wrong with the refresh token')
+    }else {
+      const accessToken = generateToken(decoded.id)
+      res.json({accessToken})
+    }
+  })
 })
 
-const getAUser = asyncHandler( async (req, res) => {
-  const {id} = req.params
+const updateAUser = asyncHandler(async (req, res) => {
   try {
-    const getUser = await User.findById(id)
-    res.json({user: getUser})
+    const { id } = req.user;
+    validateId(id);
+    const { firstName, lastName, email, mobile } = req.body;
+    const updateUser = await User.findByIdAndUpdate(
+      id,
+      {
+        firstName,
+        lastName,
+        email,
+        mobile,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({ updateUser });
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
-)
+});
 
-const deleteAUser = async (req, res) => {
-  const {id} = req.params
+
+
+
+
+
+const getAUser = asyncHandler(async (req, res) => {
+  const { id } = req.user;
   try {
-    const deleteUser = await User.findByIdAndDelete(id)
-    res.json({deleteUser})
-
+    const getUser = await User.findById(id);
+    res.json({ usersss: getUser });
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+});
 
+const deleteAUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateId(id);
+  try {
+    const deleteUser = await User.findByIdAndDelete(id);
+    res.json({ deleteUser });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-module.exports = { createUser, loginUser, getAllUser,getAUser,deleteAUser, updateAUser };
+const blockUser = async (req, res) => {
+  const { id } = req.user;
+  validateId(id);
+  try {
+    const block = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: true,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({ message: "User has been blocked", user: block });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+const unblockUser = async (req, res) => {
+  const { id } = req.user;
+  validateId(id);
+  try {
+    const unblock = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: true,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({ message: "User has been unblocked", user: unblock });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = {
+  createUser,
+  loginUser,
+  getAllUser,
+  getAUser,
+  deleteAUser,
+  updateAUser,
+  blockUser,
+  unblockUser,
+  handleRefreshToken,
+  logout
+};

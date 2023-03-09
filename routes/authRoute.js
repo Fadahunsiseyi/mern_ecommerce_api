1361f3f9e98c08a613a7f04a75650714136f316e
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-09 17:22:41 +0100

    update routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,16 +1,31 @@
-
-const express = require('express');
-const { createUser, loginUser, getAllUser, getAUser, deleteAUser, updateAUser } = require('../controllers/authController');
+const express = require("express");
+const {
+  createUser,
+  loginUser,
+  getAllUser,
+  getAUser,
+  deleteAUser,
+  updateAUser,
+  blockUser,
+  unblockUser,
+  handleRefreshToken,
+  logout,
+} = require("../controllers/authController");
+const { authMiddleware, isAdmin } = require("../middleswares/authMiddleware");
 const router = express.Router();
 
+router.post("/register", createUser);
+router.post("/login", loginUser);
 
-router.post('/register',createUser)
-router.post('/login',loginUser)
-router.get('/allUser',getAllUser)
-router.get('/:id',getAUser)
-router.delete('/:id',deleteAUser)
-router.put('/:id',updateAUser)
+router.get("/allUser", getAllUser);
+router.get("/refresh", handleRefreshToken);
+router.get("/logout",authMiddleware,logout);
+router.get("/:id", authMiddleware, isAdmin, getAUser);
 
+router.put("/edit-user", authMiddleware, updateAUser);
+router.put("/block-user/:id", authMiddleware,isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware,isAdmin, unblockUser);
 
+router.delete("/:id", deleteAUser);
 
 module.exports = router;

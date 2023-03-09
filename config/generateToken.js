7c04e76f9559213b3728393dded9dca59d962c9d
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-09 17:18:36 +0100

    add logic to make sure the function is receiving the right id

--- config/generateToken.js
+++ config/generateToken.js
@@ -1,7 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken =  (id) => {
-  return  jwt.sign({ id }, process.env.JWT, { expiresIn: "3d" });
+    if(!id){
+        throw new Error("Invalid token")
+    }else {
+       return jwt.sign({ id }, process.env.JWT, { expiresIn: "3d" });
+    }
 };
 
 module.exports = generateToken

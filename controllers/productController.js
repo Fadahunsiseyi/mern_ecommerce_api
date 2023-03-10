743421163567ba85492d8dcb9f6ba62d71d457fb
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-10 20:03:51 +0100

    add logic to create product

---
+++ controllers/productController.js
@@ -0,0 +1,18 @@
+const Product = require("../models/productModel.js");
+const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
+
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const newProduct = await Product.create(req.body);
+    res.json({ newProduct });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+
+module.exports = { createProduct, getAProduct, getAllProducts, updateAProduct,deleteProduct };

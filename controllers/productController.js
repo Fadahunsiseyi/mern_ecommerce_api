9538b6a4c34a5cc3a0b3360979d65c17d1be33b7
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-21 14:17:45 +0100

    add logic to limit the number of  products

--- controllers/productController.js
+++ controllers/productController.js
@@ -42,6 +42,14 @@ const getAllProducts = asyncHandler(async (req, res) => {
       query = query.sort("-createdAt");
     }
 
+    //Limiting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
 
 
     const products = await query;

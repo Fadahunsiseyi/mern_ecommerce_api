68dc04185b44843b3a46bdc5a30e6c6ec730cf89
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-24 14:08:09 +0100

    add the model for the blog

---
+++ models/BlogModel.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    numViews: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisLiked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    disLikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    image: {
+      type: String,
+      default: "",
+    },
+    author: {
+      type: String,
+      default: "admin",
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+//Export the model
+module.exports = mongoose.model("Blog", blogSchema);

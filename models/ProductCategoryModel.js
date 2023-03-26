5c408f9f50d5f4f8b2a987fccc8ce43ea7d1db6a
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-26 18:17:11 +0100

    add the model for the product

---
+++ models/ProductCategoryModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose'); 
+
+var productCategorySchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true,
+        index:true,
+    },
+},{
+    timestamps:true
+});
+
+//Export the model
+module.exports = mongoose.model('ProductCategory', productCategorySchema);

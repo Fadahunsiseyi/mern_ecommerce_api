731244cf9766e3c6e42b5a92dbb445560d206e7f
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-28 19:52:48 +0100

    add route to upload blog images

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -9,12 +9,22 @@ const {
   likeBlog,
   disLikeBlog,
 } = require("../controllers/blogController");
+const { blogImgResize, uploadPhoto } = require("../middleswares/uploadImages");
+const { uploadImages } = require("../controllers/blogController");
 const router = express.Router();
 
 router.put("/likes", authMiddleware, likeBlog);
 router.put("/dislikes", authMiddleware, disLikeBlog);
 router.post("/", authMiddleware, isAdmin, createBlog);
 router.put("/:id", authMiddleware, isAdmin, updateBlog);
+router.put(
+  "/upload/:id",
+  authMiddleware,
+  isAdmin,
+  uploadPhoto.array("images", 10),
+  blogImgResize,
+  uploadImages
+);
 router.get("/:id", getBlog);
 router.get("/", getAllBlog);
 router.delete("/:id", authMiddleware, isAdmin, deleteBlog);

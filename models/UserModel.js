6600073e2ed8703756846a112e3c24492fa4b6d0
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-03-29 09:56:38 +0100

    add data type for the address

--- models/UserModel.js
+++ models/UserModel.js
@@ -38,10 +38,9 @@ const userSchema = new mongoose.Schema({
         type:Boolean,
         default:false,
     },
-    address: [{
-        type:mongoose.Schema.Types.ObjectId,
-        ref: "Address",
-    }],
+    address: {
+        type: String
+    },
     wishlist: [{
         type:mongoose.Schema.Types.ObjectId,
         ref: "Product",
